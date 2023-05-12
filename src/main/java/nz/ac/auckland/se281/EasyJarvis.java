0335ce4f369079e2d8872045a550a4c8fd5f442e
jaytipattni <jayti.pattni@gmail.com>
2023-05-12 15:54:04 +1200

    Completed EasyJarvis class

--- src/main/java/nz/ac/auckland/se281/EasyJarvis.java
+++ src/main/java/nz/ac/auckland/se281/EasyJarvis.java
@@ -2,5 +2,16 @@ package nz.ac.auckland.se281;
 
 public class EasyJarvis {
   // Implements the random AI strategy
-  Strategy strategy = new Strategy(new RandomStrategy());
+
+  public int getJarvisFingers() {
+    return Utils.getRandomNumber(1, 5);
+  }
+
+  public int getJarvisSum() {
+    return Utils.getRandomNumber(2 + getJarvisFingers(), 10);
+  }
+
+  public void printJarvis() {
+    MessageCli.PRINT_INFO_HAND.printMessage("Jarvis", "%s", "%s");
+  }
 }
